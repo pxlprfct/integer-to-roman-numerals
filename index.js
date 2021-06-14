d0091cdad6ba1592182be46235452a09f46bcd22
Ollie Williams <hey@ollie.work>
2021-06-14 09:46:49 +0100

    chore: minor tidy up

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const isANumber = (input) => {
-  if (Number.isNaN(input)) { throw new TypeError(`parameter (${input}) is not a number`); }
+// NOTE: _needs_ to be in descending order
+const NUMERALS = [
+  [1_000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
+];
+
+const isNumber = (input) => {
+  if (Number.isNaN(input)) {
+    throw new TypeError(`parameter (${input}) is not a number`);
+  }
+  return true;
 };
 
 const isBetween = (min, max, input) => {
-  if (input < min || input > max) { throw new RangeError(`parameter (${input}) is out of range (0 - ${max})`); }
+  if (input < min || input > max) {
+    throw new RangeError(`parameter (${input}) is out of range (0 - ${max})`);
+  }
+  return true;
 };
 
-const integerToRomanNumeral = (integer) => {
-  // NOTE: _needs_ to be in descending order
-  const NUMERALS = [
-    [1000, 'M'],
-    [900, 'CM'],
-    [500, 'D'],
-    [400, 'CD'],
-    [100, 'C'],
-    [90, 'XC'],
-    [50, 'L'],
-    [40, 'XL'],
-    [10, 'X'],
-    [9, 'IX'],
-    [5, 'V'],
-    [4, 'IV'],
-    [1, 'I'],
-  ];
+const isBetweenZeroAndFourThousand = (input) => isBetween(0, 3999, input);
 
+const validateInput = (input) =>
+  isNumber(input) && isBetweenZeroAndFourThousand(input);
+
+const integerToRomanNumeral = (integer) => {
   let input = parseInt(integer, 10);
 
-  isANumber(input);
-  isBetween(0, 3999, input);
+  validateInput(input);
 
   if (input === 0) {
-    return 'N';
+    return "N";
   }
 
   // chunks the input down by the largest whole number that fits into it
@@ -40,7 +50,7 @@ const integerToRomanNumeral = (integer) => {
     acc.push(letter.repeat(input / number));
     input %= number;
     return acc;
-  }, []).join('');
+  }, []).join("");
 };
 
 // NOTE: I saw this a few years ago - it's quite a cool way to solve the problem
@@ -50,22 +60,23 @@ const integerToRomanNumeral = (integer) => {
 // Declarative Thinking, Declarative Practice - Kevlin Henney
 const integerToRomanNumeralRegex = (integer) => {
   if (integer === 0) {
-    return 'N';
+    return "N";
   }
 
-  return 'I'.repeat(integer)
-    .replace(/IIIII/g, 'V')
-    .replace(/IIII/g, 'IV')
-    .replace(/VV/g, 'X')
-    .replace(/VIV/g, 'IX')
-    .replace(/XXXXX/g, 'L')
-    .replace(/XXXX/g, 'XL')
-    .replace(/LL/g, 'C')
-    .replace(/LXL/g, 'XC')
-    .replace(/CCCCC/g, 'D')
-    .replace(/CCCC/g, 'CD')
-    .replace(/DD/g, 'M')
-    .replace(/DCD/g, 'CM');
+  return "I"
+    .repeat(integer)
+    .replace(/IIIII/g, "V")
+    .replace(/IIII/g, "IV")
+    .replace(/VV/g, "X")
+    .replace(/VIV/g, "IX")
+    .replace(/XXXXX/g, "L")
+    .replace(/XXXX/g, "XL")
+    .replace(/LL/g, "C")
+    .replace(/LXL/g, "XC")
+    .replace(/CCCCC/g, "D")
+    .replace(/CCCC/g, "CD")
+    .replace(/DD/g, "M")
+    .replace(/DCD/g, "CM");
 };
 
 module.exports = {
